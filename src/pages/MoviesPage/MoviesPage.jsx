c6eb95291483346d87ba7b34513686f9a775debf
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-05-02 03:12:42 +0300

    some change in movies page

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, lazy, Suspense } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useHistory } from "react-router-dom";
 import css from "./MoviesPage.module.css";
 import { getSearch } from "../../movies-api";
 const MovieList = lazy(() => import("../../components/MovieList/MovieList"));
@@ -8,48 +8,43 @@ export default function MoviesPage() {
   const [searched, setSearched] = useState([]);
   const [query, setQuery] = useState("");
   const [error, setError] = useState(false);
-  const [isSearch, setIsSearch] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const history = useHistory();
 
   useEffect(() => {
     async function fetchSearch() {
       try {
-        if (!isSearch && searchParams.has("query")) {
-          setLoading(true);
-          setQuery(searchParams.get("query"));
-          setIsSearch(true);
-        } else if (isSearch && query.trim() !== "") {
-          setLoading(true);
-          const data = await getSearch(query);
+        setLoading(true);
+        const queryParams = searchParams.get("query");
+        if (queryParams !== null) {
+          setQuery(queryParams);
+          const data = await getSearch(queryParams);
           setSearched(data);
           setError(false);
         }
       } catch {
-        setLoading(false);
         setError(true);
       } finally {
         setLoading(false);
       }
     }
     fetchSearch();
-  }, [query, isSearch, searchParams]);
+  }, [searchParams]);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     const form = event.target.elements;
     const inputValue = form.searchInput.value.trim();
     if (inputValue !== "") {
       setQuery(inputValue);
-      setIsSearch(true);
-      searchParams.set("query", inputValue);
+      setSearchParams({ query: inputValue });
     }
   }
 
   function handleChange(event) {
     setQuery(event.target.value);
-    setIsSearch(false);
-    searchParams.delete("query");
+    setSearchParams({ query: event.target.value });
   }
 
   return (
